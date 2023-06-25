a7c12c0daf6eff3116ca75037a95356ac9607092
automateyournetwork <ptcapo@gmail.com>
2023-06-25 14:24:05 -0400

    ssl time

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -6,5 +6,4 @@ python3 manage.py migrate
 python3 manage.py collectstatic --noinput
 python3 manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
 python3 manage.py search_index --rebuild -f
-#python3 manage.py runsslserver --certificate /etc/letsencrypt/live/space.automateyournetwork.ca/fullchain1.pem --key /etc/letsencrypt/live/space.automateyournetwork.ca/privkey1.pem 0.0.0.0:8000
-python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py runsslserver --certificate /etc/letsencrypt/live/space.automateyournetwork.ca/fullchain1.pem --key /etc/letsencrypt/live/space.automateyournetwork.ca/privkey1.pem 0.0.0.0:8000
