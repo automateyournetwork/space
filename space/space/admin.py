4254d1c342606377c9629392b9479051237cd4d2
automateyournetwork <ptcapo@gmail.com>
2023-07-26 16:35:19 -0400

    except#

--- space/space/admin.py
+++ space/space/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
 from . import models
-from django.views.decorators.csrf import csrf_exempt
-
-@csrf_exempt 
-def login(): pass
 
 admin.site.register(models.Space_Credentials)
--- space/space/settings.py
+++ space/space/settings.py
@@ -134,7 +134,7 @@ CORS_ALLOW_HEADERS = list(default_headers) + [
     'sentry-trace'
 ]
 
-CSRF_TRUSTED_ORIGINS = ['https://space.automateyournetwork.ca']
+CSRF_TRUSTED_ORIGINS=['https://*.automateyournetwork.ca','*']
 
 # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # SECURE_SSL_REDIRECT = True
