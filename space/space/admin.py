b0244620cc6dd7938e00f4e2af704c5ca38bf156
automateyournetwork <ptcapo@gmail.com>
2023-07-26 16:32:57 -0400

    except

--- space/space/admin.py
+++ space/space/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-
 from . import models
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt 
+def login(): pass
 
 admin.site.register(models.Space_Credentials)
